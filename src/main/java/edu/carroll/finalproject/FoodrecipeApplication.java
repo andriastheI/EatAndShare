30e576366b71b153049e44bdf74f6ed7e2461eeb
Dre Zelele <andriasmekonnenzelele@gmail.com>
2025-09-01 10:23:49 -0600

    first spring boot code for the final project

---
+++ src/main/java/edu/carroll/finalproject/FoodrecipeApplication.java
@@ -0,0 +1,13 @@
+package edu.carroll.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodrecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodrecipeApplication.class, args);
+	}
+
+}
